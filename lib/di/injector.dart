42880dad1570a2ab56bb24dd4a5a1174aac67989
Agung Nursatria <agungnursatria@gmail.com>
2019-05-22 13:42:00 +0700

    revert injector

--- lib/di/injector.dart
+++ lib/di/injector.dart
@@ -2,7 +2,6 @@ import 'package:kiwi/kiwi.dart';
 import 'package:test_app/app_store_application.dart';
 import 'package:test_app/data/db/db_appstore_repository.dart';
 import 'package:test_app/data/network/network_interface.dart';
-import 'package:test_app/data/network/network_library.dart';
 import 'package:test_app/page/home/service/home_service.dart';
 
 class InjectorContainer {
@@ -11,15 +10,9 @@ class InjectorContainer {
   void initDependencyInjection(){
     final Container container = Container();
     container.registerInstance(NetworkInterface());
-    container.registerInstance(NetworkLibrary());
     container.registerInstance(AppStoreApplication());
     container.registerFactory((c) => c.resolve<AppStoreApplication>().dbAppStoreRepository);
     container.registerFactory((c) => HomeService(c.resolve<NetworkInterface>(), c.resolve<DBAppStoreRepository>()));
-  }
-
-  NetworkLibrary getNetworkLibraryInstance(){
-    final Container container = Container();
-    return container.resolve<NetworkLibrary>();
   }
 
   HomeService getHomeServiceInstance(){
