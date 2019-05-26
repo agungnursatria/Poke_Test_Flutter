193f76e6cacf7ebe0f68e42af122f97f27d71336
Agung Nursatria <agungnursatria@gmail.com>
2019-05-26 13:27:45 +0700

    removing duplicate in main

--- lib/app.dart
+++ lib/app.dart
@@ -3,19 +3,28 @@ import 'package:flutter_stetho/flutter_stetho.dart';
 import 'package:test_app/app_store.dart';
 import 'package:test_app/config/route.dart';
 import 'package:test_app/data/model/env.dart';
+import 'package:test_app/di/injector.dart';
 import 'package:test_app/environment/env.dart';
 import 'package:test_app/page/home/home.dart';
+import 'package:test_app/utility/framework/application.dart';
 
 class App extends StatefulWidget {
   AppStore application;
   App({@required this.application}) : assert(application != null);
+
+  static void run() async {
+    final InjectorContainer injector = InjectorContainer();
+    injector.initDependencyInjection();
+    Application application = injector.getAppStoreInstance();
+    await application.onCreate();
+    runApp(App(application: application));
+  }
 
   @override
   _AppState createState() => _AppState();
 }
 
 class _AppState extends State<App> {
-
   @override
   void initState() {
     super.initState();
--- lib/main_testing.dart
+++ lib/main_testing.dart
@@ -1,17 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:test_app/app.dart';
 import 'package:test_app/environment/env.dart';
-import 'package:test_app/di/injector.dart';
-import 'package:test_app/utility/framework/application.dart';
 
 Future main() async {
   Env.isTesting();
-
-  final InjectorContainer injector = InjectorContainer();
-  injector.initDependencyInjection();
-
-  Application application = injector.getAppStoreInstance();
-  await application.onCreate();
-  
-  runApp(App(application: application,));
+  App.run();
 }
