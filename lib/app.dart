49bbbe1801440e387dc912ed01b5c0c9a522c021
Agung Nursatria <agungnursatria@gmail.com>
2019-06-24 15:17:08 +0700

    Adding firestore

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:test_app/config/route.dart';
 import 'package:test_app/page/landing/landing.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class App extends StatefulWidget {
   String initialRoute;
@@ -16,6 +17,7 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {  
   final FirebaseMessaging _fcm = FirebaseMessaging();
+  final Firestore _db = Firestore.instance;
 
   StreamSubscription iosSubscription;
 
@@ -36,32 +38,6 @@ class _AppState extends State<App> {
     _fcm.configure(
       onMessage: (Map<String, dynamic> message) async {
         print("onMessage: $message");
-        // final snackbar = SnackBar(
-        //   content: Text(message['notification']['title']),
-        //   action: SnackBarAction(
-        //     label: 'Go',
-        //     onPressed: () => null,
-        //   ),
-        // );
-
-        // Scaffold.of(context).showSnackBar(snackbar);
-        // showDialog(
-        //   context: context,
-        //   builder: (context) => AlertDialog(
-        //         content: ListTile(
-        //           title: Text(message['notification']['title']),
-        //           subtitle: Text(message['notification']['body']),
-        //         ),
-        //         actions: <Widget>[
-        //           FlatButton(
-        //             color: Colors.amber,
-        //             child: Text('Ok'),
-        //             onPressed: () => Navigator.of(context).pop(),
-        //           ),
-        //         ],
-        //       ),
-        // );
-        
         print(message['notification']['title']);
         print(message['notification']['body']);
       },
@@ -98,20 +74,20 @@ class _AppState extends State<App> {
 
     print(fcmToken);
 
-    // // Save it to Firestore
-    // if (fcmToken != null) {
-    //   var tokens = _db
-    //       .collection('users')
-    //       .document(uid)
-    //       .collection('tokens')
-    //       .document(fcmToken);
+    // Save it to Firestore
+    if (fcmToken != null) {
+      var tokens = _db
+          .collection('users')
+          .document(uid)
+          .collection('tokens')
+          .document(fcmToken);
 
-    //   await tokens.setData({
-    //     'token': fcmToken,
-    //     'createdAt': FieldValue.serverTimestamp(), // optional
-    //     'platform': Platform.operatingSystem // optional
-    //   });
-    // }
+      await tokens.setData({
+        'token': fcmToken,
+        'createdAt': FieldValue.serverTimestamp(), // optional
+        'platform': Platform.operatingSystem // optional
+      });
+    }
   }
 
   /// Subscribe the user to a topic
